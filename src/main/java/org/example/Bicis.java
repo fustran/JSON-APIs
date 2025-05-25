e9cebc6f2c03b2f8f28fed6b525389147f3dd8bc
Iván Fuster <ifussim@outlook.com>
2025-05-25 12:27:29 +0200

    Prueba json

---
+++ src/main/java/org/example/Bicis.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ToString
+public class Bicis {
+
+    private String marca;
+    private String modelo;
+    private double precio;
+    private List<String> corredores;
+
+    public Bicis(String marca, String modelo, double precio, List<String> corredores) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.corredores = new ArrayList<>(corredores);
+    }
+
+    public Bicis(){
+
+    }
+
+}
