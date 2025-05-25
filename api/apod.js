aedb044d276d3dc6205facf1c028404cb6c5555c
Iván Fuster <ifussim@outlook.com>
2025-05-26 01:10:36 +0200

    R

--- api/apod.js
+++ api/apod.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
     try {
         const nasaRes = await fetch(
-            `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&count=1`
+            `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&count=12`
         );
         if (!nasaRes.ok) {
             return res
